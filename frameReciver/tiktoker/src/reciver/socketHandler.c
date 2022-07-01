f7666e80029dcb48290b98c305e1562bcdc33997
Jorge Vulgarin <adrivulgarin@hotmail.com>
2022-07-01 02:37:52 -0500

    TIME_OUT

--- frameReciver/tiktoker/src/reciver/socketHandler.c
+++ frameReciver/tiktoker/src/reciver/socketHandler.c
@@ -14,6 +14,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+static int TIME_OUT = 10; 
+static int TIME_REMOVE = 30; 
+
 int startSocket(const int port) {
 
   struct sockaddr_in server_address;
@@ -41,27 +44,75 @@ int startSocket(const int port) {
   printf("Servidor iniciado con exito: [%d]\n", server_sockfd);
   return server_sockfd;
 }
+
+typedef struct {
+  const unsigned int seconds;
+  const int socketFd;
+  int *finished;
+  int *msgRecived;
+
+} socketTimeOutArgs;
+
+void *socketTimeOut(void *param) {
+  socketTimeOutArgs *st_ptr = (socketTimeOutArgs *)param;
+  sleep(st_ptr->seconds);
+  //if parent did not recived a message from socket while waiting
+  if (*(st_ptr->msgRecived) == 0) {
+    close(st_ptr->socketFd);
+    printf("socked %d cerrado debido a timeOut\n", st_ptr->socketFd);
+  }
+  //alert parent thread that the timer went off and the connection was closed
+  *(st_ptr->finished) = 1;
+}
+
 void *serveConection(void *param) {
 
   threadArgs *targs_ptr = (threadArgs *)param;
   const int client_sockfd = targs_ptr->sockfd;
   threadNode *tn = targs_ptr->tn;
-  circularBuffer buffer = createCircularBuffer(14 * 2);
+
+  // soket time out
+  pthread_t timeOut_pid; //
+  int msgRecived;
+  int timerFinished;
+  socketTimeOutArgs skTO_arg = {3, client_sockfd, &timerFinished, &msgRecived};
+
+  circularBuffer buffer = createCircularBuffer(14 * 5);
 
   printf("fd thread %d \n", client_sockfd);
   char msg[50];
-  for (;;) {
+  while (1) {
+    const int timerResponse =
+        pthread_create(&timeOut_pid, NULL, socketTimeOut, &skTO_arg);
+    if (timerResponse != 0) {
+      fprintf(stderr, "No se pudo iniciar timeout\n");
+      break;
+    }
+    // wait until a message arive at the socket
     const int res = recv(client_sockfd, msg, 100, 0);
-    /** printf("%ld,%s\n",strlen(msg),msg); */
-    addCircularBuffer(&buffer, msg, strlen(msg));
-    printCircularBuffer(&buffer);
-
+    // alerts a message have arrived
+    msgRecived = 1;
+    // if the timer went off
+    if (timerFinished == 1)
+      break;
+    // if the timer did not went off, kill the thread
+    else {
+      pthread_cancel(timeOut_pid);
+    }
+    // if connection was lost
     if (res == 0) {
       printf("Se perdio la conexion\n");
       break;
     }
+
+    /** printf("%ld,%s\n",strlen(msg),msg); */
+    /** printCircularBuffer(&buffer); */
+
+    addCircularBuffer(&buffer, msg, strlen(msg));
+    printf("%s\n", msg);
   }
+  // add thread to readyqueue
   addThread(targs_ptr->tq, tn);
-  printf("threadQueue tiene %d\n", targs_ptr->tq->length);
+  // colse the socket connection
   close(client_sockfd);
 }
